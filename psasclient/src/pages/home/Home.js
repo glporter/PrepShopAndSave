13c271b9dcbc7e9f966cc45e2d7c2764895a96e4
Margaret Rumble <megrumble@gmail.com>
2018-02-15 19:47:01 -0500

    added handlers to home.js

--- psasclient/src/pages/home/Home.js
+++ psasclient/src/pages/home/Home.js
@@ -4,32 +4,45 @@ import React, { Component } from 'react';
 
 
 class Home extends Component {
+    static defaultProps = {
+        onAuth() {},
+        // heading: "Welcome back.",
+        // buttonText: "Log in",
+        signIn: true,
+        // errorMessage: undefined
+
+    }
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            email: '',
+            username: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.onAuth(this.state);
+    }
+handleChange(e){
+e.preventDefault();
+this.setState({[e.target.name]: e.target.value});
+}
     getValues = () => {
 
     };
 
     render() {
+        const {email, username, password} = this.state;
+        const {signIn} = this.props;
+        if(signIn){
         return (
             <div>
-                <Content />
-            </div>
-        );
-    }
-}
-
-
-
-const Content = () => {
-    return (
-
-        <section className="hero">
+                   <section className="hero">
             <div className="container">
                 <div className="row">
                     <div className="col-md-6 col-sm-8 mx-auto">
@@ -42,15 +55,33 @@ const Content = () => {
                                     Sign up to get started with Prep Shop & Save!
                                 </p>
                                 <div className="mt-4">
-                                    <form>
+                                    <form onSubmit ={this.handleSubmit}>
                                         <div className="form-group">
-                                            <input type="email" className="form-control" name="email" value="" placeholder="Enter email address" />
+                                            <input 
+                                            type="username" 
+                                            className="form-control" 
+                                            name="username" 
+                                            value={username}    
+                                            placeholder="Create a username" 
+                                            onChange={this.handleChange}/>
                                         </div>
                                         <div className="form-group">
-                                            <input type="password" className="form-control" name="password" value="" placeholder="Enter password" />
+                                            <input 
+                                            type="email" 
+                                            className="form-control" 
+                                            name="email" 
+                                            value={email} 
+                                            placeholder="Enter email address"
+                                            onChange={this.handleChange} />
                                         </div>
                                         <div className="form-group">
-                                            <input type="password" className="form-control" name="confirm-password" value="" placeholder="Confirm password" />
+                                            <input 
+                                            type="password" 
+                                            className="form-control" 
+                                            name="password" 
+                                            value={password} 
+                                            placeholder="Enter password"
+                                            onChange={this.handleChange} />
                                         </div>
                                         <button type="submit" className="btn btn-warning btn-block">Create Account</button>
                                     </form>
@@ -76,8 +107,19 @@ const Content = () => {
                     </div>
                 </div>
             </div>
-        </section>
-    );
-};
+        </section>    
+            </div>
+        );
+    }
+}
+}
+
+
+
+
+
+
+    
+
 
 export default Home;
